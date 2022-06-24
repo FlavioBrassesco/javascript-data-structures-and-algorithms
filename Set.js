86636223af401cc7e80485def6bffe88408bb555
Flavio Brassesco <brassescoflavio@gmail.com>
2022-06-23 22:17:01 -0300

    added set operations

--- Set.js
+++ Set.js
@@ -1,7 +1,7 @@
 // javascript already has a Set class, this is to know the inner workings
 export default class Set {
   set = {};
-  size = 0;
+  size = 0; // size can also be fetched with Object.keys(set).length
 
   add(element) {
     if (!this.set[element]) {
@@ -38,5 +38,66 @@ export default class Set {
 
   has(element) {
     return this.set.hasOwnProperty(element);
+  }
+
+  union(otherSet) {
+    const newSet = new Set();
+    const values = Object.keys(this.set);
+    const otherValues = Object.keys(otherSet.set);
+
+    for (let i = 0; i < values.length; i++) {
+      newSet.add(values[i]);
+    }
+    for (let i = 0; i < otherValues.length; i++) {
+      newSet.add(otherValues[i]);
+    }
+
+    return newSet;
+  }
+
+  intersection(otherSet) {
+    const newSet = new Set();
+    const values = Object.keys(this.set);
+
+    for (let i = 0; i < values.length; i++) {
+      if (otherSet.has(values[i])) newSet.add(values[i]);
+    }
+
+    return newSet;
+  }
+
+  difference(otherSet) {
+    const newSet = new Set();
+    const values = Object.keys(this.set);
+
+    for (let i = 0; i < values.length; i++) {
+      if (!otherSet.has(values[i])) newSet.add(values[i]);
+    }
+
+    return newSet;
+  }
+
+  symmetricDifference(otherSet) {
+    const newSet = new Set();
+    const values = Object.keys(this.set);
+    const otherValues = Object.keys(otherSet.set);
+
+    for (let i = 0; i < otherValues.length; i++) {
+      if (!this.has(otherValues[i])) newSet.add(otherValues[i]);
+    }
+    for (let i = 0; i < values.length; i++) {
+      if (!otherSet.has(values[i])) newSet.add(values[i]);
+    }
+
+    return newSet;
+  }
+
+  subset(otherSet) {
+    const values = Object.keys(this.set);
+    const otherValues = Object.keys(otherSet.set);
+    for (let i = 0; i < otherValues.length; i++) {
+      if (!this.has(otherValues[i])) return false;
+    }
+    return true;
   }
 }
