123076faa8b6b952f5efa6965a9763809c2fc3d4
Flavio Brassesco <brassescoflavio@gmail.com>
2022-06-26 13:13:56 -0300

    added bredthFirstTraverse to Graph

--- Graph.js
+++ Graph.js
@@ -14,6 +14,28 @@ class Graph {
     this.adjList.get(w).push(v);
   }
 
+  breadthFirstTraverse(callback) {
+    let queue = [];
+    let colors = [];
+    queue.push(this.vertices[0]);
+    colors[this.vertices[0]] = "gray";
+
+    while (queue.length) {
+      let current = queue.shift();
+
+      let neighbors = this.adjList.get(current);
+      for (let i = 0; i < neighbors.length; i++) {
+        if (colors[neighbors[i]] === undefined) {
+          queue.push(neighbors[i]);
+          colors[neighbors[i]] = "gray";
+        }
+      }
+
+      colors[current] = "black";
+      callback(current);
+    }
+  }
+
   toString() {
     let s = "";
 
